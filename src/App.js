581b9396d58afcae8ab77e8ec2da9faf71b96446
LouisonR <louison.roger@gadz.org>
2020-03-25 16:02:32 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   const [beers, setBeers] = useState([]);
+  const [pageNumber, setPage] = useState(1)
+
 
   useEffect(() => {
-    fetch("https://api.punkapi.com/v2/beers")
+
+    fetch('https://api.punkapi.com/v2/beers?page='+pageNumber)
       .then(response => response.json())
       .then(setBeers);
-  }, []);
+  }, [pageNumber]);
+
+  const increment = () => { setPage(pageNumber + 1) }
+  const decrement = () => {if (pageNumber > 1) {setPage(pageNumber - 1)}}
+
 
   return (
-    <ul>
-      {beers.map(beer => (
-        <li key={beer.id}>{beer.name}</li>
-      ))}
-    </ul>
+    <div>
+      <ul>
+        {beers.map(beer => (
+          <li key={beer.id}>{beer.name}</li>
+        ))}
+      </ul>
+
+      <button type="button" onClick={increment}>Increment</button>
+      <button type="button" onClick={decrement}>Decrement</button>
+    </div>
   );
 }
+
+
 export default App;
