40c4652da683b4cb17ff463af9091d101651f9d7
LouisonR <louison.roger@gadz.org>
2020-03-25 15:12:18 +0100

    update app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [beers, setBeers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.punkapi.com/v2/beers")
+      .then(response => response.json())
+      .then(setBeers);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {beers.map(beer => (
+        <li key={beer.id}>{beer.name}</li>
+      ))}
+    </ul>
   );
 }
-
 export default App;
